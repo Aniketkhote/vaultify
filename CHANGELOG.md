e185112b8fb6239539d666a9e340f5cdc3826d5e
Aniket Khote <aniketkhote99@gmail.com>
2024-12-12 16:06:03 +0530

    minor chnage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.3.1
+
+- Bump version
+
 ## 1.3.0
 
 - Bump version
--- lib/vaultify.dart
+++ lib/vaultify.dart
@@ -1,6 +1,6 @@
 /// Vaultify is a versatile local storage solution designed for Flutter applications,
 /// providing efficient data persistence and seamless integration.
-library vaultify;
+library;
 
 export "package:vaultify/src/read_write_value.dart";
 export "package:vaultify/src/storage_impl.dart";
